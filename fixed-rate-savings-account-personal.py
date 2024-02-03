207ae57ccc50280a1888995fa7a450feabc60702
stefanobeni <stefanobeni44@gmail.com>
2024-02-03 18:31:25 +0000

    personal1

--- fixed-rate-savings-account-personal.py
+++ fixed-rate-savings-account-personal.py
@@ -1,40 +1,107 @@
 import numpy as np
 
-def Fixed_Rate(tot_savs, init_f):
-    mthly = init_f/12  # Amount of money you need to live for a month.
 
-    fxd_rt = 0
-    bonus = tot_savs - init_f
-    flex = init_f
+def Fixed_Rate(tot_savs, year, out_rt):
+    '''
+    It is more complicated than this because you need to consider the interest gained on the interest generated every
+    month by the Fixed Rates Savings Account.
+    '''
+    mthly = year/12  # Amount of money you need to live for a month.
 
-    int_1 = 0.00  # Interest rate of fixed rate savings account.
-    int_2 = 0.00  # Bonus interest rate of bonus savings account.
-    int_3 = 0.00  # Interest rate of bonus savings account (for withdrawal month) and flexible savings account
+    FRSA_deposit = tot_savs - year
+    OBSA_deposit = year
+    FSA_deposit = 0
 
-    bonus = bonus * (1 + int_2)
-    for j in range(12):
-        if mthly > 0:
-            fxd_rt += mthly
-            fxd_rt = fxd_rt * (1 + int_1/12)
-    for i in range(12):
-        if flex > 0:
-            flex -= mthly
-            flex = flex * (1 + int_3/12)
-        elif flex < 0:
-            print('ERROR: Negative Balance')
-    # print('... from the fixed rate account: ' + str(fxd_rt))
-    # print('... from the bonus account: ' + str(bonus))
-    # print('... from the flexible account: ' + str(flex))
-    return fxd_rt + bonus + flex
+    int_1 = 0.04  # Interest rate of bonus savings account (for withdrawal month) and flexible savings account.
+    int_2 = 0.02  # Bonus interest rate of bonus savings account.
+    int_3 = 0.05  # Interest rate of fixed rate savings account.
+
+    i = 1
+    accmltd_interest_FRSA = FRSA_deposit * int_3
+    accmltd_interest_OBSA = 0
+    accmltd_interest_FSA = 0
 
 
-total_savings = 00000       # The total amount of savings you have accumulated.
-initial_flexible = 00000    # The amount of savings you need to live for a year.
-                            # (This is because you cannot access money in
-                            # the fixed rate savings account for a whole year)
+    for element in range(12):
 
-print('Monthly addition to Fixed Account: ' + '%.2f' % (initial_flexible/12))
-print('Initial amount in the Bonus Account: ' + '%.2f' % (total_savings-initial_flexible))
-print('Initial amount in the Flexible Account: ' + '%.2f' % initial_flexible + '\n')
+        # These are the amounts in the deposits at the START of each month.
+        # print("Month = " + str(i))
+        # print("OBSA deposit = " + str(OBSA_deposit))
+        # print("FSA deposit  = " + str(FSA_deposit))
+
+        if FSA_deposit >= mthly:
+            """
+            Scenario 1: Enough money in the FSA_deposit to last the month. OBSA account will have higher interest rate.
+            """
+            OBSA_mnthly_interest = OBSA_deposit * int_1 / 12
+            FSA_mnthly_interest = (FSA_deposit - mthly) * int_2 / 12
+
+            OBSA_deposit += OBSA_mnthly_interest
+            FSA_deposit += FSA_mnthly_interest
+
+            accmltd_interest_OBSA += OBSA_mnthly_interest
+            accmltd_interest_FSA += FSA_mnthly_interest
+
+            FSA_deposit -= mthly
+            i += 1
+
+        elif FSA_deposit < mthly and OBSA_deposit > (mthly*out_rt):
+            """
+            Scenario 2: Not enough money in the FSA account to last the month. Need to transfer money from OBSA account.
+                        OBSA account will have lower interest rate.
+            """
+            FSA_deposit  += (mthly * out_rt)
+            OBSA_deposit -= (mthly * out_rt)
+
+            OBSA_mnthly_interest = OBSA_deposit * int_2 / 12
+            FSA_mnthly_interest  = (FSA_deposit - mthly) * int_2 / 12
+
+            OBSA_deposit += OBSA_mnthly_interest
+            FSA_deposit  += FSA_mnthly_interest
+
+            accmltd_interest_OBSA += OBSA_deposit * int_2 / 12
+            accmltd_interest_FSA  += (FSA_deposit - mthly) * int_2 / 12
+
+            # print("WITHDRAW")
+            FSA_deposit -= mthly
+            i += 1
+
+        elif FSA_deposit < mthly:
+            """
+            Scenario 3: Not enough money in the FSA account to last the month. Final transfer as money in OBSA account 
+                        is less then (mnthly * out_rt). All money in OBSA account is transferred to FSA account.
+            """
+            FSA_deposit = OBSA_deposit
+            OBSA_deposit = 0
+
+            FSA_mnthly_interest = (FSA_deposit - mthly) * int_2 / 12
+            FSA_deposit += FSA_mnthly_interest
+            accmltd_interest_FSA += (FSA_deposit - mthly) * int_2 / 12
+
+            # print("WITHDRAW TOTAL")
+            FSA_deposit -= mthly
+            i += 1
+
+        else:
+            print('ERROR: Negative deposit')
+
+    output = {"Final FRSA deposit": FRSA_deposit,
+              "Final OBSA deposit": OBSA_deposit,
+              "Final FSA deposit": FSA_deposit,
+              "Accumulated FRSA interest": accmltd_interest_FRSA,
+              "Accumulated OBSA interest": accmltd_interest_OBSA,
+              "Accumulated FSA interest": accmltd_interest_FSA}
+    return output
+
+
+total_savings = 12000     # The total amount of savings you have accumulated.
+year_need = 3000          # The amount of savings you need to live for a year.
+                          # (This is because you cannot access money in
+                          # the fixed rate savings account for a whole year)
+OBSA_withdrawal_rate = 3  # This is the withdrawal rate. In other words it is the number of months you withdraw
+                          # money for in the event that you withdraw.
+
+results = Fixed_Rate(total_savings, year_need, OBSA_withdrawal_rate)
+
 print('With these parameters, the amount in your savings account after a year is '
-      + '%.2f' % Fixed_Rate(total_savings, initial_flexible))
+      + '%.2f' % (results["Final FRSA deposit"]+results["Final OBSA deposit"]+results["Final FSA deposit"]))
